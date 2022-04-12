7c9f4aad3d7e5bb8f69e1817c84ee5331683fa57
Gaojianhua <jianhua.gao@foxmail.com>
2022-04-12 16:06:26 +0800

    init

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="flex h-screen items-center justify-center dark:bg-[#111827] dark:text-[#fff]">
+      <div className="font-mono text-2xl font-bold">NextJs & TailWindCss</div>
+    </div>
+  );
+}
