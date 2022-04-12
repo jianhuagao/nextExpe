1ed18b2e844ac3bdb3a2e093a6054856cd44eacd
Gaojianhua <jianhua.gao@foxmail.com>
2022-04-12 16:23:24 +0800

    home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
 export default function Index() {
   return (
-    <div className="flex h-screen items-center justify-center dark:bg-[#111827] dark:text-[#fff]">
-      <div className="font-mono text-2xl font-bold">
-        <a href="https://nextjs.org/">nextJs</a>
-        <span className="mx-3">&</span>
-        <a href="https://tailwindcss.com/" className="underline decoration-sky-500">
-          tailwindcss
-        </a>
+    <div className="flex h-screen items-center justify-center dark:bg-[#111827] dark:text-white">
+      <div>
+        <div className="text-center text-4xl font-extrabold sm:text-5xl lg:text-6xl">
+          <a href="https://nextjs.org/">nextJs</a>
+          <span className="mx-3">&</span>
+          <a href="https://tailwindcss.com/" className="underline decoration-sky-500">
+            tailwindcss
+          </a>
+        </div>
+        <p className="mx-auto mt-6 max-w-3xl text-center text-lg text-slate-600 dark:text-slate-400">
+          a nextjs + tailwindcss <code className="font-mono font-medium text-sky-500 dark:text-sky-400">react</code> boilerplate
+        </p>
       </div>
     </div>
   );
