7190759735bc0a09c3a03ff000955bff595e5647
Gaojianhua <jianhua.gao@foxmail.com>
2022-04-12 16:13:20 +0800

    change

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 export default function Index() {
   return (
     <div className="flex h-screen items-center justify-center dark:bg-[#111827] dark:text-[#fff]">
-      <div className="font-mono text-2xl font-bold">NextJs & TailWindCss</div>
+      <div className="font-mono text-2xl font-bold">
+        <a href="https://nextjs.org/">nextJs</a>
+        <span className="mx-3">&</span>
+        <a href="https://tailwindcss.com/" className="underline decoration-sky-500">
+          tailwindcss
+        </a>
+      </div>
     </div>
   );
 }
