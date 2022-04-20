bb44d7999437eb6ff177f2d79efe5f0510b46184
jianhuagao <jianhua.gao@foxmail.com>
2022-04-20 23:54:52 +0800

    Changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,7 +47,7 @@ export default function Index() {
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <div className="my-8 backdrop-blur bg-white/60 inline-block w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+              <div className="my-8 backdrop-blur bg-white/60 inline-block w-full max-w-md transform overflow-hidden rounded-2xl p-6 text-left align-middle shadow-xl transition-all">
                 <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
                   Payment successful
                 </Dialog.Title>
